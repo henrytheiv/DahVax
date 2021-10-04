7ae22db6bbc9b74abe74a5ccc038ca3f798c67a8
henrytheiv <henrytheiv@users.noreply.github.com>
2021-10-04 16:45:25 +0800

    add pop-up window

--- scripts.js
+++ scripts.js
@@ -2,3 +2,10 @@ function dropDownIconChange() {
     $(".dropdown-toggle i").toggleClass("fa-bars fa-caret-down");
 } 
 
+function openLoginForm(){
+    document.body.classList.add("showPopUp");
+  }
+  function closeLoginForm(){
+    document.body.classList.remove("showPopUp");
+  }
+
