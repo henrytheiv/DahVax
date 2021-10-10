0442e90e5da7fb4a51dbb04b0baa8b058d9b2c5d
henrytheiv <henrytheiv@users.noreply.github.com>
2021-10-10 16:52:28 +0800

    modify CSS

--- scripts.js
+++ scripts.js
@@ -1,11 +1,28 @@
-function dropDownIconChange() { 
-    $(".dropdown-toggle i").toggleClass("fa-bars fa-caret-down");
-} 
+function dropDownIconChange() {
+  $(".dropdown-toggle i").toggleClass("fa-bars fa-caret-down");
+}
 
-function openLoginForm(){
-    document.body.classList.add("showPopUp");
-  }
+function openPopUp() {
+  document.body.classList.add("showPopUp");
+}
 
-function closeLoginForm(){
-    document.body.classList.remove("showPopUp");
-  }
+function closePopUp() {
+  document.body.classList.remove("showPopUp");
+}
+
+function openAdminSignUpForm() {
+  document.body.classList.add("showPopUp-admin-sign-up");
+}
+
+function closeAdminSignUpForm() {
+  document.body.classList.remove("showPopUp-admin-sign-up");
+}
+
+function openManageVaccination(){
+  document.body.classList.add("showPopUp-manage-vaccination");
+}
+
+function closeManageVaccination(){
+  document.body.classList.remove("showPopUp-manage-vaccination");
+}
+
