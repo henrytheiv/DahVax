2da511d3644f3e1dce8bc0febb22b89b47bcc6b3
henrytheiv <henrytheiv@users.noreply.github.com>
2021-10-10 16:44:56 +0800

    new sign up page

--- scripts.js
+++ scripts.js
@@ -1,4 +1,21 @@
-function dropDownIconChange() { 
-    $(".dropdown-toggle i").toggleClass("fa-bars fa-caret-down");
-} 
+function dropDownIconChange() {
+  $(".dropdown-toggle i").toggleClass("fa-bars fa-caret-down");
+}
+
+function openPopUp() {
+  document.body.classList.add("showPopUp");
+}
+
+function closePopUp() {
+  document.body.classList.remove("showPopUp");
+}
+
+function openAdminSignUpForm() {
+  document.body.classList.add("showPopUp-admin-sign-up");
+}
+
+function closeAdminSignUpForm() {
+  document.body.classList.remove("showPopUp-admin-sign-up");
+}
+
 
