baa76b54bedd3326c15a6f8c118291aa098a250f
henrytheiv <henrytheiv@users.noreply.github.com>
2021-10-12 17:30:46 +0800

    modify login page

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@ function dropDownIconChange() {
   $(".dropdown-toggle i").toggleClass("fa-bars fa-caret-down");
 }
 
-
 //the pop up for patient sign up (OPEN)
 function openPatientSignUpForm() {
   document.body.classList.add("showPopUp-patient-sign-up");
@@ -33,12 +32,6 @@ function closePopUp() {
   document.body.classList.remove("showPopUp");
 }
 
-
-
-
-
-
-
 //requestvaccineappointment.html
 //the pop up for request vaccine (show centre name) (OPEN)
 function openPopUpRequestVaccine1() {
@@ -53,9 +46,7 @@ function closePopUpRequestVaccine1() {
 
 //requestvaccineappointment.html
 //the confrim button after user select the date (quit pop up and show details outside pop up)
-function confirmAppointment() {
-}
-
+function confirmAppointment() {}
 
 //requestvaccineappointment.html
 //hiding and showing the Batch No table
@@ -79,23 +70,7 @@ function showCalender() {
   }
 }
 
-function openManageVaccination(){
-  document.body.classList.add("showPopUp-manage-vaccination");
-}
-
-function closeManageVaccination(){
-  document.body.classList.remove("showPopUp-manage-vaccination");
-}
-
-
-
-
-
-
-
-
-
-//havent done this 
+//havent done this
 //open the centre name and show name and address in sign up
 function openCentreName() {
   document.body.classList.add("showPopUp-centre-name");
@@ -105,4 +80,61 @@ function closeCentreName() {
   document.body.classList.close("showPopUp-centre-name");
 }
 
+// Login validation
 
+const loginForm = document.getElementById("form");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkLoginDetails();
+});
+
+function checkLoginDetails() {
+  usernameValue = username.value.trim();
+  passwordValue = password.value.trim();
+
+
+  if (usernameValue === '') {
+    setErrorFor(username, "Username cannot be blank");
+  }
+
+  if (passwordValue === '') {
+    setErrorFor(password, "Password cannot be blank");
+  }
+
+
+  if (usernameValue !== "abc" || passwordValue !=="123") {
+    setErrorFor(username, "User hasn't been registered");
+    setErrorFor(password, "User hasn't been registered");
+
+  } else {
+    setSuccessFor(username);
+    setSuccessFor(password);
+    location.href = "AdminMenu.html";
+  }
+
+  if (usernameValue !== "def" || passwordValue !=="456") {
+    setErrorFor(username, "User hasn't been registered");
+    setErrorFor(password, "User hasn't been registered");
+
+  } else {
+    setSuccessFor(username);
+    setSuccessFor(password);
+    location.href = "PatientMenu.html";
+  }
+
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+  formControl.className = "form-control error";
+  small.innerText = message;
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
