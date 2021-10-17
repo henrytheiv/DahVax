963a94de12d7185e02bfa5afc9f2b8fe5fbe92e2
henrytheiv <henrytheiv@users.noreply.github.com>
2021-10-17 13:14:59 +0800

    modify DahVax iteration 1 CSS and JavaScript

--- scripts.js
+++ scripts.js
@@ -25,6 +25,11 @@ function closeAdminSignUpForm() {
 //the pop up for administrator to record new vaccine (OPEN)
 function openPopUp() {
   document.body.classList.add("showPopUp");
+
+    clearBatchInputs();
+    Array.from(document.querySelectorAll(".form-control")).forEach((el) =>
+      el.classList.remove("success")
+    );
 }
 
 //the pop up for administrator to record new vaccine (CLOSE)
@@ -34,19 +39,16 @@ function closePopUp() {
 
 //requestvaccineappointment.html
 //the pop up for request vaccine (show centre name) (OPEN)
-function openPopUpRequestVaccine1() {
-  document.body.classList.add("showPopUp-requestvaccine-centre");
+function openPopUpRequestVaccination() {
+  document.body.classList.add("showPopUp-requestvaccination-centre");
 }
 
 //requestvaccineappointment.html
 //the pop up for request vaccine (show centre name) (CLOSE)
-function closePopUpRequestVaccine1() {
-  document.body.classList.remove("showPopUp-requestvaccine-centre");
+function closePopUpRequestVaccination() {
+  document.body.classList.remove("showPopUp-requestvaccination-centre");
 }
 
-//requestvaccineappointment.html
-//the confrim button after user select the date (quit pop up and show details outside pop up)
-function confirmAppointment() {}
 
 //requestvaccineappointment.html
 //hiding and showing the Batch No table
@@ -70,7 +72,6 @@ function showCalender() {
   }
 }
 
-//havent done this
 //open the centre name and show name and address in sign up
 function openCentreName() {
   document.body.classList.add("showPopUp-centre-name");
@@ -80,51 +81,44 @@ function closeCentreName() {
   document.body.classList.close("showPopUp-centre-name");
 }
 
+
 // Login validation
 
-const loginForm = document.getElementById("form");
-const username = document.getElementById("username");
-const password = document.getElementById("password");
+function loginValidation() {
+  var loginForm = document.getElementById("form");
+  var username = document.getElementById("username");
+  var password = document.getElementById("password");
 
-loginForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  checkLoginDetails();
-});
+  loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkLoginDetails();
+  });
+}
 
 function checkLoginDetails() {
   usernameValue = username.value.trim();
   passwordValue = password.value.trim();
 
-
-  if (usernameValue === '') {
-    setErrorFor(username, "Username cannot be blank");
-  }
-
-  if (passwordValue === '') {
-    setErrorFor(password, "Password cannot be blank");
-  }
-
-
-  if (usernameValue !== "abc" || passwordValue !=="123") {
-    setErrorFor(username, "User hasn't been registered");
-    setErrorFor(password, "User hasn't been registered");
-
-  } else {
+  if (usernameValue === "abc" && passwordValue === "123") {
     setSuccessFor(username);
     setSuccessFor(password);
     location.href = "AdminMenu.html";
-  }
-
-  if (usernameValue !== "def" || passwordValue !=="456") {
-    setErrorFor(username, "User hasn't been registered");
-    setErrorFor(password, "User hasn't been registered");
-
-  } else {
+  } else if (usernameValue === "def" && passwordValue === "456") {
     setSuccessFor(username);
     setSuccessFor(password);
     location.href = "PatientMenu.html";
+  } else {
+    setErrorFor(username, "Invalid username");
+    setErrorFor(password, "Invalid password");
   }
 
+  if (usernameValue === "") {
+    setErrorFor(username, "Username cannot be blank");
+  }
+
+  if (passwordValue === "") {
+    setErrorFor(password, "Password cannot be blank");
+  }
 }
 
 function setErrorFor(input, message) {
@@ -137,4 +131,234 @@ function setErrorFor(input, message) {
 function setSuccessFor(input) {
   const formControl = input.parentElement;
   formControl.className = "form-control success";
+}
+
+function patientSignUpValidation() {
+  var patientSignUpForm = document.getElementById("patient-sign-up-form");
+  var patientUsername = document.getElementById("patientUsername");
+  var patientPassword = document.getElementById("patientPassword");
+  var patientFullname = document.getElementById("patientFullname");
+  var patientEmail = document.getElementById("patientEmail");
+  var patientIcpassport = document.getElementById("patientIcpassport");
+
+  patientSignUpForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkPatientSignUpDetails();
+  });
+}
+
+function checkPatientSignUpDetails() {
+  patientUsernameValue = patientUsername.value.trim();
+  patientPasswordValue = patientPassword.value.trim();
+  patientFullnameValue = patientFullname.value.trim();
+  patientEmailValue = patientEmail.value.trim();
+  patientIcpassportValue = patientIcpassport.value.trim();
+
+  //username
+  if (patientUsernameValue === "") {
+    setErrorFor(patientUsername, "Username cannot be empty");
+  } else {
+    setSuccessFor(patientUsername);
+  }
+
+  if (patientPasswordValue === "") {
+    setErrorFor(patientPassword, "Password cannot be empty");
+  } else {
+    setSuccessFor(patientPassword);
+  }
+
+  if (patientFullnameValue === "") {
+    setErrorFor(patientFullname, "Full name cannot be empty");
+  } else {
+    setSuccessFor(patientFullname);
+  }
+
+  if (patientEmailValue === "") {
+    setErrorFor(patientEmail, "Email cannot be empty");
+  } else {
+    setSuccessFor(patientEmail);
+  }
+
+  if (patientIcpassportValue === "") {
+    setErrorFor(patientIcpassport, "Passport No. cannot be empty");
+  } else {
+    setSuccessFor(patientIcpassport);
+  }
+}
+
+//sign up validation for admin
+function adminSignUpValidation() {
+  var adminForm = document.getElementById("admin-sign-up-form");
+  var adminUsername = document.getElementById("adminUsername");
+  var adminPassword = document.getElementById("adminPassword");
+  var adminFullname = document.getElementById("adminFullname");
+  var adminEmail = document.getElementById("adminEmail");
+  var adminStaffID = document.getElementById("adminStaffID");
+
+  adminForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    adminUsernameValue = adminUsername.value.trim();
+    adminPasswordValue = adminPassword.value.trim();
+    adminFullnameValue = adminFullname.value.trim();
+    adminEmailValue = adminEmail.value.trim();
+    adminStaffIDValue = adminStaffID.value.trim();
+
+    if (adminUsernameValue === "") {
+      setErrorFor(adminUsername, "Username cannot be empty");
+    } else {
+      setSuccessFor(adminUsername);
+    }
+
+    if (adminPasswordValue === "") {
+      setErrorFor(adminPassword, "Password cannot be empty");
+    } else {
+      setSuccessFor(adminPassword);
+    }
+
+    if (adminFullnameValue === "") {
+      setErrorFor(adminFullname, "Full name cannot be empty");
+    } else {
+      setSuccessFor(adminFullname);
+    }
+
+    if (adminEmailValue === "") {
+      setErrorFor(adminEmail, "Email cannot be empty");
+    } else {
+      setSuccessFor(adminEmail);
+    }
+
+    if (adminStaffIDValue === "") {
+      setErrorFor(adminStaffID, "Staff ID cannot be empty");
+    } else {
+      setSuccessFor(adminStaffID);
+    }
+  });
+}
+
+//add new centre validation
+function addCentreValidation() {
+  var centreForm = document.getElementById("new-centre-form");
+  var centreName = document.getElementById("centreName");
+  var address = document.getElementById("address");
+
+  centreForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    centreNameValue = centreName.value.trim();
+    addressValue = address.value.trim();
+
+    if (centreNameValue === "") {
+      setErrorFor(centreName, "enter a centre name");
+    } else {
+      setSuccessFor(centreName);
+    }
+
+    if (addressValue === "") {
+      setErrorFor(address, "enter an address for centre");
+    } else {
+      setSuccessFor(address);
+    }
+  });
+}
+
+//SignUp.html
+//hiding and showing the details of centre
+function centreDetails() {
+  var x = document.getElementById("centreDetails");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+// SignUp.html
+//the pop up for add new centre  (OPEN)
+function openNewCentre() {
+  document.body.classList.add("showPopUp-add-new-centre");
+}
+
+// SignUp.html
+//the pop up for add new centre (CLOSE)
+function closeNewCentre() {
+  document.body.classList.remove("showPopUp-add-new-centre");
+}
+
+// SignUp.html
+//the pop up for administrator sign up  (OPEN)
+function openAdminSignUpForm() {
+  document.body.classList.add("showPopUp-admin-sign-up");
+}
+
+// SignUp.html
+//close the pop up for administrator sign up (CLOSE)
+function closeAdminSignUpForm() {
+  document.body.classList.remove("showPopUp-admin-sign-up");
+}
+
+function newBatchValidation() {
+  var form = document.getElementById("form");
+  var batchNo = document.getElementById("batchNo");
+  var expiryDate = document.getElementById("expiryDate");
+  var quantity = document.getElementById("quantity");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (checkBatchInfo() !== false) {
+      setTimeout(function () {
+        closePopUp();
+      }, 3000);
+      alert("Recorded successfully!");
+    }
+  });
+}
+
+function checkBatchInfo() {
+  batchNoValue = batchNo.value.trim();
+  expiryDateValue = expiryDate.value;
+  quantityValue = quantity.value.trim();
+  var correctBatch = true;
+
+  var todayDate = new Date();
+
+  if (new Date(expiryDateValue).getTime() <= todayDate.getTime()) {
+    setErrorFor(expiryDate, "Invalid expiry date");
+    correctBatch = false;
+  } else {
+    setSuccessFor(expiryDate);
+  }
+
+  if (quantityValue < 10) {
+    setErrorFor(quantity, "Invalid quantity");
+    correctBatch = false;
+  } else {
+    setSuccessFor(quantity);
+  }
+
+  if (batchNoValue === "") {
+    setErrorFor(batchNo, "Batch No. cannot be blank");
+    correctBatch = false;
+  } else {
+    setSuccessFor(batchNo);
+  }
+
+  if (expiryDateValue === "") {
+    setErrorFor(expiryDate, "Expiry date cannot be blank");
+    correctBatch = false;
+  }
+
+  if (quantityValue === "") {
+    setErrorFor(quantity, "Quantity cannot be blank");
+    correctBatch = false;
+  }
+
+  return correctBatch;
+}
+
+function clearBatchInputs() {
+  document.getElementById("batchNo").value = "";
+  document.getElementById("expiryDate").value = "";
+  document.getElementById("quantity").value = "";
 }
